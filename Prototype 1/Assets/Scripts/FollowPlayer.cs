030e41f67278cace5b4d20bbb3bdad57dccc58c3
Shadezy <Travis_Cartmell@hotmail.com>
2020-02-16 13:46:05 +0900

    first commit

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 camera_offset = new Vector3(0, 7.58f, -8.14f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = player.transform.position + camera_offset;
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 10.0f;
+    private float turn_speed = 35.0f;
+    private float horizontal_input;
+    private float forward_input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal_input = Input.GetAxis("Horizontal");
+        forward_input = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forward_input);
+        transform.Rotate(Vector3.up * Time.deltaTime * turn_speed * horizontal_input);
+    }
+}
